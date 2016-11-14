019c83b80b5e49a346212d9658648ba8735079ec
AD004SM <AD004SM@UK-B010LC2.pbi.global.pvt>
2016-11-15 00:51:29 +0100

    healthcheck fixed

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -2,9 +2,9 @@
 
 for i in `seq 1 10`;
 do
-  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:8080/mm`
+  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:8080/mm/`
   if [ "$HTTP_CODE" == "200" ]; then
-    echo "Successfully pulled root page."
+    echo "Successfully pulled messenger page."
     exit 0;
   fi
   echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying."
